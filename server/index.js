0e69931fc78782816b456775f4ce2bdae3146139
pratikkadam00 <pratikadam00@gmail.com>
2022-11-25 16:03:15 +0530

    new commit

--- server/index.js
+++ server/index.js
@@ -7,13 +7,14 @@ import userRoutes from './routes/users.js'
 import questionRoutes from './routes/Questions.js'
 import answerRoutes from './routes/Answers.js'
 
+//const DATABASE_URL = "mongodb+srv://admin:pass@stack-overflow-clone.v8rekmi.mongodb.net/?retryWrites=true&w=majority"
 const app = express();
 dotenv.config();
-app.use(express.json({limit: "30mb", extended: true}))
-app.use(express.urlencoded({limit: "30mb", extended: true}))
+app.use(express.json({ limit: "30mb", extended: true }))
+app.use(express.urlencoded({ limit: "30mb", extended: true }))
 app.use(cors());
 
-app.get('/',(req, res) => {
+app.get('/', (req, res) => {
     res.send("This is a stack overflow clone API")
 })
 
@@ -23,8 +24,10 @@ app.use('/answer', answerRoutes)
 
 const PORT = process.env.PORT || 5000
 
+//const CONNECTION_URL = "mongodb+srv://admin:pass@stack-overflow-clone.v8rekmi.mongodb.net/?retryWrites=true&w=majority"
+
 const DATABASE_URL = process.env.CONNECTION_URL
 
-mongoose.connect( DATABASE_URL, { useNewUrlParser: true, useUnifiedTopology: true})
-    .then(() => app.listen(PORT, () => {console.log(`server running on port ${PORT}`)}))
+mongoose.connect(DATABASE_URL, { useNewUrlParser: true, useUnifiedTopology: true })
+    .then(() => app.listen(PORT, () => { console.log(`server running on port ${PORT}`) }))
     .catch((err) => console.log(err.message))
